bf5e45a031d73aa29639ec9ca6e63d88d7b4f009
sangeetha <jadamalsangeetha@gmail.com>
2024-04-11 00:36:49 +0530

    modified: hero, navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Hero from './components/hero.jsx'
+import Navbar from './components/navbar.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Hero />
+    <Navbar />
   </React.StrictMode>,
 )
